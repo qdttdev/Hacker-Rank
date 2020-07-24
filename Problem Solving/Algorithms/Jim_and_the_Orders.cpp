31cad6bbd2e4e8dbd384421a995d0cfaccb13904
Caroline Ta <qdttdev@gmail.com>
2020-07-24 11:48:23 -0700

    Update Jim and the Orders of 07/24/2020

--- Problem Solving/Algorithms/Jim_and_the_Orders.cpp
+++ Problem Solving/Algorithms/Jim_and_the_Orders.cpp
@@ -2,29 +2,43 @@
 
 using namespace std;
 
-// TEST CASE 7 doesn't work, suspecting sum value needs to hold a long instead of an int.
-
-// Driver function to sort the 2D vector base on 2nd column 
-bool sortCol(const vector<int>& v1, const vector<int>& v2) 
+// Driver function to sort the 2D vector base on 3rd column 
+bool sortCol3(const vector<int>& v1, const vector<int>& v2) 
 { 
-    return v1[1] < v2[1]; 
+    return v1[2] < v2[2]; 
 } 
 
 // Complete the jimOrders function below.
 vector<int> jimOrders(vector<vector<int>> orderPrep) // [order[i],prep[i]]
 {
     // Note: Sizing vector when declaring --> orderServe(rowSize, colSize)
-    vector<vector<int>> orderServe(orderPrep.size(), vector<int>(2)); // [customer[i],serve[i]]
+    vector<vector<int>> orderServe(orderPrep.size(), vector<int>(3));
     vector<int> orderDelivered;
 
-    for(int i = 0; i < orderPrep.size(); i++)
+    // Populate orderServe: [customer[i], order[i], serveTime[i]]
+    for(int i = 0; i < orderServe.size(); i++)
     {        
         orderServe[i][0] = i+1;
-        orderServe[i][1] = orderPrep[i][0] + orderPrep[i][1];
+        orderServe[i][1] = orderServe[i][0];
+        orderServe[i][2] = orderPrep[i][0] + orderPrep[i][1];
     }
 
-    sort(orderServe.begin(), orderServe.end(), sortCol);
+    // Sort orderServe array by serveTime at column 3
+    sort(orderServe.begin(), orderServe.end(), sortCol3);
 
+    // Handle case at the same serveTime to follow order # 
+    for(int i = 0; i < orderServe.size() - 1; i++)
+    {
+        if((orderServe[i][2] == orderServe[i+1][2])
+        && (orderServe[i][1] > orderServe[i+1][1]))
+        {
+            swap(orderServe[i][0],orderServe[i+1][0]);
+            swap(orderServe[i][1],orderServe[i+1][1]);
+            swap(orderServe[i][2],orderServe[i+1][2]);
+        }
+    }
+
+    // Populate orderDeliver array
     for(int i = 0; i < orderPrep.size(); i++)
     {
         orderDelivered.push_back(orderServe[i][0]);
