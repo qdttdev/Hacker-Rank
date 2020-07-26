7e0bb8e0bc474c93a0fe810e146368d6efe61042
Caroline Ta <qdttdev@gmail.com>
2020-07-25 23:54:14 -0700

    Start on Super Reduced String

---
+++ Problem Solving/Algorithms/Super_Reduced_String.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the superReducedString function below.
+string superReducedString(string s) 
+{
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = superReducedString(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
