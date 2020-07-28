2005298f81d2d803b21509ebc0ba44e7cfa154cd
Caroline Ta <qdttdev@gmail.com>
2020-07-27 22:06:31 -0700

    Update Super Reduced String (failed 4/16 tests)

--- Problem Solving/Algorithms/Super_Reduced_String.cpp
+++ Problem Solving/Algorithms/Super_Reduced_String.cpp
@@ -3,12 +3,38 @@
 using namespace std;
 
 // PROBLEM: Cannot handle case "baab" where "bb" is not handled
+// TEST CASES FAILED: 11, 13, 14, 15
 
 // Complete the superReducedString function below.
 string superReducedString(string s) 
 {
+    string r = "";
 
+    for(int i = 0; i < s.length() - 1; i++)
+    {
+        if(s[i] == s[i+1])
+        {
+            s[i] = '_';
+            s[i+1] = '_';
+        }
+    }
 
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] != '_')
+        {
+            r += s[i];
+        }
+
+        cout << "NEW STRING: " << r << endl;
+    }
+
+    if(r[0] == r[1])
+    {
+        r = "Empty String";
+    } 
+
+    return r;
 }
 
 int main()
