2ac6b0a3d385b34a5964bda7356d0ad237592d82
Caroline Ta <qdttdev@gmail.com>
2020-08-01 14:50:47 -0700

    Add Day 3: Intro to Conditional Statements

---
+++ 30_Days_of_Code/Day_3_Intro_to_Conditional_Statements.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if(N % 2 != 0) // odd
+    {
+        cout << "Weird";
+    }
+    else // even
+    {
+        if(N >= 6 && N <= 20)
+        {
+            cout << "Weird";
+        }
+        else 
+        {
+            cout << "Not Weird";
+        }
+    }
+
+    return 0;
+}
