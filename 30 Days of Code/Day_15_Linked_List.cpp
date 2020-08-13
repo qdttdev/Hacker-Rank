56f37925d88ee460d112fddbefa456d7e4ed0c5d
Caroline Ta <qdttdev@gmail.com>
2020-08-12 20:29:58 -0700

    Create Day_15_Linked_List.cpp

---
+++ 30 Days of Code/Day_15_Linked_List.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+        Node* insert(Node *head,int data)
+        {
+            Node *newNode = new Node(data);
+            Node *curr = head;
+
+            if(head == NULL)
+            {
+                head = newNode;
+            }
+            else 
+            {
+                while(curr->next != NULL)
+                {
+                    curr = curr->next;
+                }
+
+                curr->next = newNode;
+            }           
+
+            return head;
+        }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
