5193a19cc4765e8968c1b3554ca1e170a5d2e757
Caroline Ta <qdttdev@gmail.com>
2020-08-14 15:36:32 -0700

    Create Day_25_Running_Time_and_Complexity.cpp.cpp

---
+++ 30 Days of Code/Day_25_Running_Time_and_Complexity.cpp.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int testCases = 0;
+    int testInput = 0;    
+
+    cin >> testCases;
+
+    for(int i = 0; i < testCases; i++)
+    {        
+        cin >> testInput;
+
+        if(testInput <= 1)
+        {
+            cout << "Not prime\n";
+        } 
+        else 
+        {
+            int count = 0;
+
+            for(int j = 2; j <= sqrt(testInput); j++)
+            {
+                if(testInput % j == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+
+            if(count > 0)
+            {
+                cout << "Not prime\n";
+            }
+            else 
+            {
+                cout << "Prime\n";
+            }
+        }
+    }
+
+    return 0;
+}
