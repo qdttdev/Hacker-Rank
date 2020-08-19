981f6b94f0045fd28df265f51c14c723d7a056cf
Caroline Ta <qdttdev@gmail.com>
2020-08-18 23:41:22 -0700

    Update Picking_Numbers.cpp

--- Problem Solving/Algorithms/Picking_Numbers.cpp
+++ Problem Solving/Algorithms/Picking_Numbers.cpp
@@ -15,17 +15,21 @@ vector<string> split(const string &);
 
 int pickingNumbers(vector<int> a) 
 {
+    // Objective: We sort the array so that when comparing with base value, it satisfies the condition: diff <= 1
+    // It is ensured that the value to compare to the base value is greater than or equal to base value
+    // If the condition fails, start on the next base value
+    // By "count"-ing the number of values that satisfies "diff <= 1", we can find the "max" value exists, which is the size of the subarray
+    
     int max = 0;
     int count = 0;
-    sort(a.begin(),a.end());
+    sort(a.begin(), a.end());
 
-    for(int i = 0; i < a.size(); i++)
+    for(int i = 0; i < a.size(); i++) // going through every element as base value
     {
-        count = 1;
+        count = 1; // the base value is the first element in the subarray
 
-        for(int j = i+1; j < a.size(); j++)
+        for(int j = i+1; j < a.size(); j++) // comparing these elements to base value
         {
-
             if(abs(a[i]-a[j]) <= 1)
             {
                 count++;
@@ -33,6 +37,10 @@ int pickingNumbers(vector<int> a)
                 {
                     max = count;
                 }
+            }
+            else // if condition doesn't meet, move on to the next base value
+            {
+                break;
             }
         }
     }
