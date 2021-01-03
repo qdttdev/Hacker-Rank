a1b00e7387265a6787e10e59fc2a731a273636de
Caroline Ta <qdttdev@gmail.com>
2021-01-02 18:29:01 -0800

    Add Circular Array Rotation

---
+++ Problem Solving/Algorithms/Circular_Array_Rotation.cpp
@@ -0,0 +1,117 @@
+// How to insert an element into a vector:
+// https://www.geeksforgeeks.org/vector-insert-function-in-c-stl/
+
+// How to erase an element in a vector:
+// https://www.geeksforgeeks.org/vector-erase-and-clear-in-cpp/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the circularArrayRotation function below.
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) 
+{
+    vector<int> returnValue;
+    
+    // Rotate the vector 'k' times by inserting last element 
+    // to the beginning of the vector and erasing the last element.
+    for(int i = 0; i < k; i++)
+    {
+        a.insert(a.begin(), a.at(a.size()-1));
+        a.erase(a.begin() + a.size()-1);
+    }
+    
+    // Report indices, taking values of queries as the index to report
+    for(int i = 0; i < queries.size(); i++)
+    {
+        returnValue.push_back(a.at(queries.at(i)));
+    }
+    
+    return returnValue;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nkq_temp;
+    getline(cin, nkq_temp);
+
+    vector<string> nkq = split_string(nkq_temp);
+
+    int n = stoi(nkq[0]);
+
+    int k = stoi(nkq[1]);
+
+    int q = stoi(nkq[2]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    vector<int> queries(q);
+
+    for (int i = 0; i < q; i++) {
+        int queries_item;
+        cin >> queries_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> result = circularArrayRotation(a, k, queries);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
